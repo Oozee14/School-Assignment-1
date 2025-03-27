d6867ecba379db8d54d3461b375eadd519f1aae2
Oozee14 <choutdin@gmail.com>
2025-03-27 18:39:59 +0700

    Delete WebHW_Test - Copy directory

--- WebHW_Test - Copy/Asset/Javascript/Dashboard.js
+++
@@ -1,5 +0,0 @@
-document.addEventListener("DOMContentLoaded", function() {
-    document.getElementById("nav-button").addEventListener("click", function() {
-        window.location.href = "../index.html";
-    });
-});
--- WebHW_Test - Copy/test_func/pop_up.js
+++
@@ -1,5 +0,0 @@
-// Function to toggle the popup menu visibility
-function toggleNav() {
-    const menu = document.getElementById("popupMenu");
-    menu.classList.toggle("open");
-}
