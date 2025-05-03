95a2ed18f2c8b928bc89749ab64e9ff54e2074ab
Oozee14 <choutdin@gmail.com>
2025-05-03 21:58:21 +0700

    All the page setup are done

--- WebHW_Test/Asset/Javascript/Nav_bar.js
+++ WebHW_Test/Asset/Javascript/Nav_bar.js
@@ -1,4 +1,15 @@
+fetch('../../Pages/Navbar.html')
+.then(res => res.text())
+.then(html => {
+    document.getElementById('navbar').innerHTML = html;
+});
+
+fetch('../../Pages/Sidebar.html')
+.then(res => res.text())
+.then(html => {
+    document.getElementById('side-bar').innerHTML = html;
+});
 function OpenSidebar() {
-    const div = document.querySelector(".side-bar");
+    const div = document.querySelector('.side-bar');
     div.style.display = (div.style.display === "none" || div.style.display === "")? "block" : "none";
-}
+};
